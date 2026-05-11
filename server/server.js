6c179ed2504ae95d4154ea29c08a1fb60e8bbabe
DHANWANTH01 <dhanwanthb@gmaiL.com>
2026-05-11 20:28:25 +0530

    commit changes made

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "MyOwn@13579@",
-  database: "portfolio_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
